5b8340c922c5e451b0c37b2f1111556bfd91fe64
kimhnh <kimhnh@gmail.com>
2023-12-14 18:39:02 +0900

    refactor

---
+++ src/NotesForm.jsx
@@ -0,0 +1,20 @@
+import Input from './components/Input';
+
+// WIP WIP WIP ///////////////////////////////////
+export function NotesForm({}) {
+  return (
+    <form action="">
+      <h3>My Notes (WIP)</h3>
+      <Input>Notes...</Input>
+      <button>+</button>
+    </form>
+  );
+}
+// Re-use List Component?
+export function NoteList() {
+  return (
+    <ul>
+      <li>placeholder</li>
+    </ul>
+  );
+}
